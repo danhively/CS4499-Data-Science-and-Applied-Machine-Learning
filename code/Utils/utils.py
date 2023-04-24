9730b1be3ddab6816b6f73204f864e4f4ae7fd56
Dan Hively <dan@hively.io>
2023-04-24 17:27:55 -0600

    Update utils.py

--- code/Utils/utils.py
+++ code/Utils/utils.py
@@ -4,24 +4,24 @@
 
 # Function to rename a column by position
 def rename_column_by_position(df, position, new_name):
-    if 0 <= position < len(df.columns):
-        columns = df.columns.tolist()
-        columns[position] = new_name
-        df.columns = columns
-    else:
-        raise ValueError(f"Invalid position. DataFrame has {len(df.columns)} columns.")
+  if 0 <= position < len(df.columns):
+      columns = df.columns.tolist()
+      columns[position] = new_name
+      df.columns = columns
+  else:
+      raise ValueError(f"Invalid position. DataFrame has {len(df.columns)} columns.")
 
 # Drop specified columns from a DataFrame.
 def drop_columns(df, columns_to_drop):
-    return df.drop(columns=columns_to_drop)        
+  return df.drop(columns=columns_to_drop)        
 
 # Reads a CSV file from the given URL and returns a pandas DataFrame.
 def read_csv_to_dataframe(csv_url):
-    return pd.read_csv(csv_url)    
+  return pd.read_csv(csv_url)    
 
 # Reads a CSV file from the given URL and returns a pandas DataFrame including features.
 def read_csv_to_dataframe_w_features(csv_url, features):
-    return pd.read_csv(csv_url, features=feature_names)    
+  return pd.read_csv(csv_url, features=feature_names)    
 
 # Print the shape of the DataFrame
 def print_shape(df):
